a32df0167616e3683b77cc40f01741d234765285
Nathan da Silva Barrozo <email.do.natann@gmail.com>
2023-10-03 21:06:46 -0300

    10-03-2023

--- Arvore/src/NoBin.java
+++ Arvore/src/NoBin.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class NoBin {
+public class NoBin<T> {
+
+    private T conteudo;
+    private NoBin<T> noBinEsq;
+    private NoBin<T> noBinDir;
+
+    NoBin(T conteudo){
+        this.conteudo = conteudo;
+        this.noBinEsq = this.noBinDir = null;
+    }
+
+    public NoBin<T> getNoBinEsq() {
+        return noBinEsq;
+    }
+
+    public void setNoBinEsq(NoBin<T> noBinEsq) {
+        this.noBinEsq = noBinEsq;
+    }
+
+    public NoBin<T> getNoBinDir() {
+        return noBinDir;
+    }
+
+    public void setNoBinDir(NoBin<T> noBinDir) {
+        this.noBinDir = noBinDir;
+    }
+
+    @Override
+    public String toString() {
+        return "NoBin{" +
+                "conteudo=" + conteudo +
+                '}';
+    }
 }
