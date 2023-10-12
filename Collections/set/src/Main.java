e6ba4a1ca51ab4859b75b8ef854ee2edcd64df31
Nathan da Silva Barrozo <email.do.natann@gmail.com>
2023-10-12 20:42:33 -0300

    12-10-2023-1

---
+++ Collections/set/src/Main.java
@@ -0,0 +1,64 @@
+import javax.annotation.processing.SupportedSourceVersion;
+import java.sql.SQLSyntaxErrorException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Crie um conjunto e adicione as notas: ");
+        Set<Double> notas = new HashSet<>(Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6));
+        System.out.println(notas.toString());
+
+        System.out.println("Confira se a nota 5 está no conjunto: " + notas.contains(5d) );
+
+        System.out.println("Exiba o menor nota recebida: " + Collections.min(notas));
+
+        System.out.println("Exiba a maior nota recebido: " + Collections.max(notas));
+
+        Iterator<Double> iterator = notas.iterator();
+        Double soma = 0.0;
+        while (iterator.hasNext()){
+            Double next = iterator.next();
+            soma +=next;
+        }
+
+        System.out.println("Exibir a soma dos valores:" + soma);
+
+        System.out.println("Exibir a media das notas: " + (soma/notas.size()));
+
+        System.out.println("Remova a nota: 0");
+        notas.remove(0d);
+        System.out.println(notas);
+
+        System.out.println("Remova os valores menores que 7:");
+
+        Iterator<Double> iterator1 = notas.iterator();
+        while (iterator1.hasNext()){
+            Double next = iterator1.next();
+            if(next < 7) iterator1.remove();
+        }
+        System.out.println(notas);
+
+        System.out.println("Exiba todas as notas na ordem em que foram informadas:");
+
+        Set<Double> notas2 = new LinkedHashSet<>();
+
+        notas2.add(7d);
+        notas2.add(8.5);
+        notas2.add(9.3);
+        notas2.add(5d);
+        notas2.add(0d);
+        notas2.add(3.6);
+        System.out.println(notas2);
+
+        System.out.println("Exiba todas as notas na ordem crescente:");
+        Set<Double> notas3 = new TreeSet<>(notas2);
+        System.out.println(notas3);
+
+//        System.out.println("Apague todo o conjunto");
+//        notas.clear();
+//
+//        System.out.println(notas);
+
+    }
+}
