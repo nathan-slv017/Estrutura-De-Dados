d66862d8504dc128b9eef4b2f570bc56b34c9fd8
Nathan da Silva Barrozo <email.do.natann@gmail.com>
2023-10-18 20:38:27 -0300

    18-10-2023-4

--- praticando-17-10-2023/listaDuplamenteEncadeada/src/one/digitalinnovation/ListaDuplamenteEncadeada.java
+++ praticando-17-10-2023/listaDuplamenteEncadeada/src/one/digitalinnovation/ListaDuplamenteEncadeada.java
@@ -1,2 +1,42 @@
-package one.digitalinnovation;public class ListaDuplamenteEncadeada {
+package one.digitalinnovation;
+
+public class ListaDuplamenteEncadeada<T> {
+
+    private NoDuplo<T> primeiroNo;
+    private NoDuplo<T> ultimoNo;
+
+    private int tamanhoLista = 0;
+
+    public void add(T elemento){
+        NoDuplo<T> novoNo = new NoDuplo(elemento);
+        novoNo.setNoProximo(null);
+        novoNo.setNoPrevio(ultimoNo);
+        if(primeiroNo == null){
+            primeiroNo = novoNo;
+        }
+        if(ultimoNo != null){
+            ultimoNo.setNoProximo(novoNo);
+        }
+        ultimoNo = novoNo;
+        tamanhoLista ++;
+    }
+
+    private NoDuplo<T> getNo(int index){
+        NoDuplo<T> noAuxiliar = primeiroNo;
+        for(int i = 0; (i < index) && (noAuxiliar != null); i++){
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        return noAuxiliar;
+    }
+
+    
+
+    public T get(int index){
+        return getNo(index).getConteudo();
+    }
+
+    public int size(){
+        return this.tamanhoLista;
+    }
+
 }
