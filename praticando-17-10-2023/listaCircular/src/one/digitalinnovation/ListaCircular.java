9cb005fd7144639263679d1529dbc91b953cc979
Nathan da Silva Barrozo <email.do.natann@gmail.com>
2023-10-19 20:11:59 -0300

    19-10-2023-4

--- praticando-17-10-2023/listaCircular/src/one/digitalinnovation/ListaCircular.java
+++ praticando-17-10-2023/listaCircular/src/one/digitalinnovation/ListaCircular.java
@@ -1,2 +1,82 @@
-package one.digitalinnovation;public class ListaCircular {
+package one.digitalinnovation;
+
+public class ListaCircular<T> {
+
+    private No<T> cabeca;
+    private No<T> cauda;
+    private int tamanhoLista;
+
+    ListaCircular(){
+        cabeca = null;
+        cauda = null;
+        tamanhoLista = 0;
+    }
+
+    public void add(T element){
+        No<T> novoNo = new No<>(element);
+        if(tamanhoLista == 0){
+            this.cabeca = novoNo;
+            this.cauda = this.cabeca;
+            this.cabeca.setProximoNo(this.cabeca);
+        } else{
+            novoNo.setProximoNo(this.cauda);
+            this.cabeca.setProximoNo(novoNo);
+            this.cauda = novoNo;
+        }
+        tamanhoLista ++;
+    }
+
+    public void remove(int index){
+        if(index >= this.tamanhoLista)
+            throw new IndexOutOfBoundsException("O índice maior que o tamanho da lista");
+
+        No<T> noAuxiliar = cauda;
+        if(index == 0){ // estou na cauda
+            this.cauda = this.cauda.getProximoNo();
+            this.cabeca.setProximoNo(cauda);
+        } else if(index == 1){
+            this.cauda.setProximoNo(this.cauda.getProximoNo().getProximoNo());
+        } else {
+            for(int i = 0; i < index -1; i++){
+                noAuxiliar = noAuxiliar.getProximoNo();
+            }
+            noAuxiliar.setProximoNo(noAuxiliar.getProximoNo().getProximoNo());
+        }
+        this.tamanhoLista --;
+    }
+
+    public boolean isEmpty(){
+        return this.tamanhoLista == 0;
+    }
+    public int size(){
+        return this.tamanhoLista;
+    }
+
+    private No<T> get(int index){
+        if(isEmpty())
+            throw new IndexOutOfBoundsException("A lista está vazia");
+
+        if (index == 0)
+            return this.cauda;
+
+        No<T> noAuxiliar = this.cauda;
+
+        for(int i = 0; (i < index) && (noAuxiliar != null); i++){
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+        return noAuxiliar;
+    }
+
+    @Override
+    public String toString() {
+        String strRetorno = "";
+        No<T> noAuxiliar = this.cauda;
+        for(int i = 0; i < size(); i++){
+            strRetorno += noAuxiliar;
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+
+        strRetorno += size() != 0? "(Retorna ao início)" : "[]";
+        return strRetorno;
+    }
 }
--- praticando-17-10-2023/listaCircular/src/one/digitalinnovation/No.java
+++ praticando-17-10-2023/listaCircular/src/one/digitalinnovation/No.java
@@ -1,2 +1,35 @@
-package one.digitalinnovation;public class No {
+package one.digitalinnovation;
+
+public class No<T> {
+
+    private T conteudo;
+    private No<T> proximoNo;
+
+    No(T conteudo){
+        this.conteudo = conteudo;
+    }
+
+    public T getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(T conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public No<T> getProximoNo() {
+        return proximoNo;
+    }
+
+    public void setProximoNo(No<T> proximoNo) {
+        this.proximoNo = proximoNo;
+    }
+
+    @Override
+    public String toString() {
+        return "No{" +
+                "conteudo=" + conteudo +
+                ", proximoNo=" + proximoNo +
+                '}';
+    }
 }
