9552949457db067e1be3dc1996aa76d4d7931550
Nathan da Silva Barrozo <email.do.natann@gmail.com>
2023-10-29 18:26:16 -0300

    29-10-2023-1

--- praticando-17-10-2023/untitled/src/BinNo.java
+++ praticando-17-10-2023/untitled/src/BinNo.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class BinNo {
+public class BinNo <T extends Comparable<T>> {
+
+    private T conteudo;
+    private BinNo<T> noEsq;
+    private BinNo<T> noDir;
+
+    public BinNo(T conteudo){
+        this.conteudo = conteudo;
+        this.noDir = this.noEsq = null;
+    }
 }
