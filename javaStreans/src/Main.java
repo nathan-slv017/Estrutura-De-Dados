f9c6db083e656367891c1931fd8d33d340d0b1ac
Nathan da Silva Barrozo <email.do.natann@gmail.com>
2023-10-31 21:21:35 -0300

    31-10-2023-2

--- javaStreans/src/Main.java
+++ javaStreans/src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Nodes.collect;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> numerosAleatorios =
+                Arrays.asList("1", "0", "4", "1", "2", "3", "9", "9", "6", "5");
+
+        System.out.println("Imprima todos os elementos dessa lsita de String: ");
+
+        numerosAleatorios.forEach(System.out::println);
+
+        System.out.println("Pegue os 5 primeiros números e coloque dentro de um Set:");
+
+        numerosAleatorios.stream()
+                .limit(5)
+                .collect(Collectors.toSet())
+                .forEach(System.out::println);
+
+
+        System.out.println("Transforme esta lista de String em uma lista de números inteiros.");
+        numerosAleatorios.stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+        
+        List<Integer> ListParesMaioresQue2 = numerosAleatorios.stream()
+                .map(Integer::parseInt)
+                .filter(i -> (i %2 ==0 && i > 2)).collect(Collectors.toList());
     }
+
 }
